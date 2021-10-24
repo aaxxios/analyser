e439a7e4a626728b27184620c2770934e59767de
axios <jamespenabas8@gmail.com>
2021-10-24 08:00:26 +0100

    initial

--- analyser/parser.py
+++ analyser/parser.py
@@ -1,14 +1,17 @@
 import argparse
 from analyser import anal
+import textwrap
 
-
+epilog = textwrap.dedent("""
+Author: axios ::
+email: xxxjamesfoil@gmail.com""")
 def main():
 	parser = argparse.ArgumentParser(description="""
-		Scan files for emails, ipv4 addreses, mac addresses and urls""")
+		Scan files for emails, ipv4 addreses, mac addresses and urls""", epilog=epilog)
 	exclusive = parser.add_mutually_exclusive_group(required=True)
-	exclusive.add_argument('-f', "--file",  metavar='<filename>', type=str, nargs='+', help='files to analyse')
-	parser.add_argument('-o', "--output", dest='output')
-	exclusive.add_argument("-i", "--inline", dest="inline", type=str, nargs="+")
+	exclusive.add_argument('-f', "--file",  metavar='<filename>', type=str, nargs='+', help='files to analyse\n')
+	parser.add_argument('-o', "--output", dest='output', help="File to write output of analysis to. If not given, content will be written to stdout")
+	exclusive.add_argument("-i", "--inline", dest="inline", nargs="+",)
 	args = parser.parse_args()
 	
 	if args.inline:
